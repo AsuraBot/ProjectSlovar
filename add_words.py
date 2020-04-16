4e4558426911b98180cb4aa5680e536bb140120a
AsuraBot <homecat13@gmail.com>
2020-04-16 19:02:59 +0300

    '16.04'

--- add_words.py
+++ add_words.py
@@ -1,8 +1,9 @@
 with open('bigdic.txt', 'r', encoding='utf8') as f:
-    count = 0
     buffer = []
     main_words = {}
-    sub_words = {}
+    sub_word = {}
+    word = []
+    sub_words = []
     while True:
         line = f.readline()
         if line != ' \n':
@@ -11,17 +12,43 @@ with open('bigdic.txt', 'r', encoding='utf8') as f:
         else:
             if len(buffer) == 12:
                 for item in buffer:
+                    s_item = item.split('|')
+                    word = s_item[0].strip()
+                    options = s_item[1].strip().split()
                     if item.startswith('  '):
-                        s_item = item.split('|')
-                        print('  ', s_item[0].strip())
+                        if not main_words.get('word_plural'):
+                            sub_word['word'] = word
+                            sub_word['word_case'] = options[-1]
+                            sub_words.append(sub_word.copy())
+                        else:
+
+                            sub_word['word_plural'] = word
+                            print('  ', word, options)
                     else:
-                        count += 1
-                        s_item = item.split('|')
-                        print(s_item[0].strip())
+                        if not main_words.get('word'):
+                            sub_word['word'] = word
+                            sub_word['word_case'] = options[-1]
+                            sub_words.append(sub_word.copy())
+
+                            main_words['word'] = word
+                            main_words['is_nominal'] = 1
+                            if options[3] == 'муж':
+                                main_words['gender'] = 'М'
+                            elif options[3] == 'жен':
+                                main_words['gender'] = 'Ж'
+                            else:
+                                main_words['gender'] = 'С'
+                            if options[1] == 'неод':
+                                main_words['is_animate'] = 0
+                            else:
+                                main_words['is_animate'] = 1
+                        else:
+                            main_words['word_plural'] = word
+                        print(word, options)
             else:
                 pass
             buffer = []
-        if count < 0:
+        if 1 < 0:
             break
 
-#TODO Собрать данные о слове (род, падеж, неодушевленнность)
+#TODO Убрать два поля, доделать это дерьмо нах
